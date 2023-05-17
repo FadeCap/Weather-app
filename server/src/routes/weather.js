553ca5240624c5b566dfffaef4d2eb8e9e08c0cf
Salamon Rørvik <74314359+Sallis-GH@users.noreply.github.com>
2023-05-17 23:22:21 +0200

    Revert "reverse pull"

--- server/src/routes/weather.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import weatherController from '../controllers/weatherController.js';
-
-const router = express.Router();
-
-// Define route
-router.get('/', weatherController.getWeather);
-
-export default router;
